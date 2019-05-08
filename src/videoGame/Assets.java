725e21caae33264275b75e65921989a33c6f8a83
Alvaro Santana <alvaro.santana23@gmail.com>
2019-05-07 20:34:28 -0500

    Added level 1 2 and 3

--- src/videoGame/Assets.java
+++ src/videoGame/Assets.java
@@ -53,7 +53,7 @@ public class Assets {
      */
     public static void init(){
         background = ImageLoader.loadImage("/images/background.png"); 
-        map = ImageLoader.loadImage("/images/Map2.png");
+        map = ImageLoader.loadImage("/images/Level2.png");
         
         wall = ImageLoader.loadImage("/images/Wall.png");
         alien = ImageLoader.loadImage("/images/alien.png"); 
